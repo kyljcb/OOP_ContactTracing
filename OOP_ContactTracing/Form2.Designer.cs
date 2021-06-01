d6690b5fe93225f389a1e276973286b4ff7a63cb
Kyle Jacob <kylelesterjacob17@gmail.com>
2021-06-01 13:58:26 +0800

    Draft layout for form 2

--- OOP_ContactTracing/Form2.Designer.cs
+++ OOP_ContactTracing/Form2.Designer.cs
@@ -29,20 +29,233 @@ namespace OOP_ContactTracing
         /// </summary>
         private void InitializeComponent()
         {
+            this.tb_Q1 = new System.Windows.Forms.TextBox();
+            this.tb_Q2 = new System.Windows.Forms.TextBox();
+            this.tb_Q3 = new System.Windows.Forms.TextBox();
+            this.tb_Q4 = new System.Windows.Forms.TextBox();
+            this.tb_Q5 = new System.Windows.Forms.TextBox();
+            this.label_Yes = new System.Windows.Forms.Label();
+            this.label_No = new System.Windows.Forms.Label();
+            this.rb_Q1Yes = new System.Windows.Forms.RadioButton();
+            this.rb_Q1No = new System.Windows.Forms.RadioButton();
+            this.rb_Q2Yes = new System.Windows.Forms.RadioButton();
+            this.rb_Q2No = new System.Windows.Forms.RadioButton();
+            this.rb_Q3Yes = new System.Windows.Forms.RadioButton();
+            this.rb_Q3No = new System.Windows.Forms.RadioButton();
+            this.rb_Q4Yes = new System.Windows.Forms.RadioButton();
+            this.rb_Q4No = new System.Windows.Forms.RadioButton();
+            this.rb_Q5Yes = new System.Windows.Forms.RadioButton();
+            this.rb_Q5No = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // tb_Q1
+            // 
+            this.tb_Q1.Location = new System.Drawing.Point(12, 45);
+            this.tb_Q1.Multiline = true;
+            this.tb_Q1.Name = "tb_Q1";
+            this.tb_Q1.Size = new System.Drawing.Size(498, 59);
+            this.tb_Q1.TabIndex = 0;
+            // 
+            // tb_Q2
+            // 
+            this.tb_Q2.Location = new System.Drawing.Point(12, 110);
+            this.tb_Q2.Multiline = true;
+            this.tb_Q2.Name = "tb_Q2";
+            this.tb_Q2.Size = new System.Drawing.Size(498, 59);
+            this.tb_Q2.TabIndex = 1;
+            // 
+            // tb_Q3
+            // 
+            this.tb_Q3.Location = new System.Drawing.Point(11, 175);
+            this.tb_Q3.Multiline = true;
+            this.tb_Q3.Name = "tb_Q3";
+            this.tb_Q3.Size = new System.Drawing.Size(498, 40);
+            this.tb_Q3.TabIndex = 2;
+            this.tb_Q3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tb_Q4
+            // 
+            this.tb_Q4.Location = new System.Drawing.Point(11, 221);
+            this.tb_Q4.Multiline = true;
+            this.tb_Q4.Name = "tb_Q4";
+            this.tb_Q4.Size = new System.Drawing.Size(498, 40);
+            this.tb_Q4.TabIndex = 5;
+            // 
+            // tb_Q5
+            // 
+            this.tb_Q5.Location = new System.Drawing.Point(12, 267);
+            this.tb_Q5.Multiline = true;
+            this.tb_Q5.Name = "tb_Q5";
+            this.tb_Q5.Size = new System.Drawing.Size(498, 62);
+            this.tb_Q5.TabIndex = 6;
+            // 
+            // label_Yes
+            // 
+            this.label_Yes.AutoSize = true;
+            this.label_Yes.Location = new System.Drawing.Point(567, 19);
+            this.label_Yes.Name = "label_Yes";
+            this.label_Yes.Size = new System.Drawing.Size(30, 20);
+            this.label_Yes.TabIndex = 7;
+            this.label_Yes.Text = "Yes";
+            // 
+            // label_No
+            // 
+            this.label_No.AutoSize = true;
+            this.label_No.Location = new System.Drawing.Point(628, 19);
+            this.label_No.Name = "label_No";
+            this.label_No.Size = new System.Drawing.Size(29, 20);
+            this.label_No.TabIndex = 8;
+            this.label_No.Text = "No";
+            // 
+            // rb_Q1Yes
+            // 
+            this.rb_Q1Yes.AutoSize = true;
+            this.rb_Q1Yes.Location = new System.Drawing.Point(567, 69);
+            this.rb_Q1Yes.Name = "rb_Q1Yes";
+            this.rb_Q1Yes.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q1Yes.TabIndex = 9;
+            this.rb_Q1Yes.TabStop = true;
+            this.rb_Q1Yes.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q1No
+            // 
+            this.rb_Q1No.AutoSize = true;
+            this.rb_Q1No.Location = new System.Drawing.Point(628, 69);
+            this.rb_Q1No.Name = "rb_Q1No";
+            this.rb_Q1No.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q1No.TabIndex = 10;
+            this.rb_Q1No.TabStop = true;
+            this.rb_Q1No.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q2Yes
+            // 
+            this.rb_Q2Yes.AutoSize = true;
+            this.rb_Q2Yes.Location = new System.Drawing.Point(567, 134);
+            this.rb_Q2Yes.Name = "rb_Q2Yes";
+            this.rb_Q2Yes.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q2Yes.TabIndex = 11;
+            this.rb_Q2Yes.TabStop = true;
+            this.rb_Q2Yes.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q2No
+            // 
+            this.rb_Q2No.AutoSize = true;
+            this.rb_Q2No.Location = new System.Drawing.Point(628, 134);
+            this.rb_Q2No.Name = "rb_Q2No";
+            this.rb_Q2No.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q2No.TabIndex = 12;
+            this.rb_Q2No.TabStop = true;
+            this.rb_Q2No.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q3Yes
+            // 
+            this.rb_Q3Yes.AutoSize = true;
+            this.rb_Q3Yes.Location = new System.Drawing.Point(567, 180);
+            this.rb_Q3Yes.Name = "rb_Q3Yes";
+            this.rb_Q3Yes.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q3Yes.TabIndex = 13;
+            this.rb_Q3Yes.TabStop = true;
+            this.rb_Q3Yes.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q3No
+            // 
+            this.rb_Q3No.AutoSize = true;
+            this.rb_Q3No.Location = new System.Drawing.Point(628, 180);
+            this.rb_Q3No.Name = "rb_Q3No";
+            this.rb_Q3No.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q3No.TabIndex = 14;
+            this.rb_Q3No.TabStop = true;
+            this.rb_Q3No.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q4Yes
+            // 
+            this.rb_Q4Yes.AutoSize = true;
+            this.rb_Q4Yes.Location = new System.Drawing.Point(567, 226);
+            this.rb_Q4Yes.Name = "rb_Q4Yes";
+            this.rb_Q4Yes.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q4Yes.TabIndex = 15;
+            this.rb_Q4Yes.TabStop = true;
+            this.rb_Q4Yes.UseVisualStyleBackColor = true;
+            this.rb_Q4Yes.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // rb_Q4No
+            // 
+            this.rb_Q4No.AutoSize = true;
+            this.rb_Q4No.Location = new System.Drawing.Point(628, 226);
+            this.rb_Q4No.Name = "rb_Q4No";
+            this.rb_Q4No.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q4No.TabIndex = 16;
+            this.rb_Q4No.TabStop = true;
+            this.rb_Q4No.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q5Yes
+            // 
+            this.rb_Q5Yes.AutoSize = true;
+            this.rb_Q5Yes.Location = new System.Drawing.Point(567, 287);
+            this.rb_Q5Yes.Name = "rb_Q5Yes";
+            this.rb_Q5Yes.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q5Yes.TabIndex = 17;
+            this.rb_Q5Yes.TabStop = true;
+            this.rb_Q5Yes.UseVisualStyleBackColor = true;
+            // 
+            // rb_Q5No
+            // 
+            this.rb_Q5No.AutoSize = true;
+            this.rb_Q5No.Location = new System.Drawing.Point(628, 287);
+            this.rb_Q5No.Name = "rb_Q5No";
+            this.rb_Q5No.Size = new System.Drawing.Size(17, 16);
+            this.rb_Q5No.TabIndex = 18;
+            this.rb_Q5No.TabStop = true;
+            this.rb_Q5No.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(694, 450);
+            this.Controls.Add(this.rb_Q5No);
+            this.Controls.Add(this.rb_Q5Yes);
+            this.Controls.Add(this.rb_Q4No);
+            this.Controls.Add(this.rb_Q4Yes);
+            this.Controls.Add(this.rb_Q3No);
+            this.Controls.Add(this.rb_Q3Yes);
+            this.Controls.Add(this.rb_Q2No);
+            this.Controls.Add(this.rb_Q2Yes);
+            this.Controls.Add(this.rb_Q1No);
+            this.Controls.Add(this.rb_Q1Yes);
+            this.Controls.Add(this.label_No);
+            this.Controls.Add(this.label_Yes);
+            this.Controls.Add(this.tb_Q5);
+            this.Controls.Add(this.tb_Q4);
+            this.Controls.Add(this.tb_Q3);
+            this.Controls.Add(this.tb_Q2);
+            this.Controls.Add(this.tb_Q1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_Q1;
+        private System.Windows.Forms.TextBox tb_Q2;
+        private System.Windows.Forms.TextBox tb_Q3;
+        private System.Windows.Forms.TextBox tb_Q4;
+        private System.Windows.Forms.TextBox tb_Q5;
+        private System.Windows.Forms.Label label_Yes;
+        private System.Windows.Forms.Label label_No;
+        private System.Windows.Forms.RadioButton rb_Q1Yes;
+        private System.Windows.Forms.RadioButton rb_Q1No;
+        private System.Windows.Forms.RadioButton rb_Q2Yes;
+        private System.Windows.Forms.RadioButton rb_Q2No;
+        private System.Windows.Forms.RadioButton rb_Q3Yes;
+        private System.Windows.Forms.RadioButton rb_Q3No;
+        private System.Windows.Forms.RadioButton rb_Q4Yes;
+        private System.Windows.Forms.RadioButton rb_Q4No;
+        private System.Windows.Forms.RadioButton rb_Q5Yes;
+        private System.Windows.Forms.RadioButton rb_Q5No;
     }
 }
--- OOP_ContactTracing/Form2.cs
+++ OOP_ContactTracing/Form2.cs
@@ -19,6 +19,20 @@ namespace OOP_ContactTracing
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            tb_Q1.Text = "Have you visited any affected geographic area / country within the last 30 days?";
+            tb_Q2.Text = "Have you attended a mass gathering, party or reunion with relatives or friends within the last 30 days?";
+            tb_Q3.Text = "Have you been in close contact with a COVID-19 positive patient?";
+            tb_Q4.Text = "Have you been in close contact with a person under investigation (PUI)?";
+            tb_Q5.Text = "Do you have any flu-like / respiratory symptoms? (eg. fever, cough, runny nose, sore throat, headache, shortness of breath.)";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
 
         }
     }
